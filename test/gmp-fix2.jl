3dd57630887b550203746ee998138f111b3c5ea4
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2024-05-14 12:25:21 +0200

    gmp reg

--- test/gmp-fix2.jl
+++ test/gmp-fix2.jl
@@ -104,14 +104,14 @@ b = monomials(x, 0:approximation_degree(m))
 dbdt = differentiate(b, x) * f(x,u)
 
 @variable m μ[i=1:length(K),j=1:3] Meas([x;u], support=K[i][j])
-@objective m Min sum(Mom.(c(x,u), μ[:,2]))
+@objective m Min sum(Mom.(c(x,u), μ[:,2])) + 0.1*sum(Mom.(1, μ[:,1] + μ[:,3]))
 @constraint m [i=1:length(K)] Mom.(dbdt, μ[i,2]) .== Mom.(b, μ[i,3]) - Mom.(b, μ[i,1])
 @constraint m [i=1:_modes] sum(μ[eout(i),1]) == sum(μ[einc(i),3])
 @constraint m sum(μ[eout(nmodes(hs)+1),1]) == μ0
 @constraint m sum(μ[einc(nmodes(hs)+2),3]) == μT
 
 optimize!(m)
-println(objective_value(m))
+println(sum(integrate.(c(x,u), μ[:,2])))
 p = integrate.(1,μ[:,1])
 t = integrate.(1,μ[:,2])
 o = integrate.(1,μ[:,3])
