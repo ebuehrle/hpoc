64641c925174564971731aa05199576d5b0474fb
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2024-05-21 13:44:05 +0200

    group measures

--- test/gmp2.jl
+++ test/gmp2.jl
@@ -1,5 +1,6 @@
 using MomentOpt, DynamicPolynomials, MosekTools
 using Plots
+using DifferentialEquations
 
 eout(E,i) = E[:,1] .== i
 einc(E,i) = E[:,2] .== i
@@ -52,10 +53,13 @@ K = [
 
 A = [0 0 1 0; 0 0 0 1; 0 0 0 0; 0 0 0 0]
 B = [0 0; 0 0; 1 0; 0 1]
-f = A*x + B*u
+x0 = [-1.0, -1.0, 0.0, 0.5]
+xT = [-0.0, -0.0, 0.0, 0.0]
 c = x'*x + u'*u
-μ0 = DiracMeasure(x, [-1.0, -1.0, 0.0, 0.5])
-μT = DiracMeasure(x, [-0.0, -0.0, 0.0, 0.0])
+
+f = A*x + B*u
+μ0 = DiracMeasure(x, x0)
+μT = DiracMeasure(x, xT)
 
 m = GMPModel(Mosek.Optimizer)
 set_approximation_mode(m, PRIMAL_RELAXATION_MODE())
@@ -64,24 +68,32 @@ b = monomials(x, 0:approximation_degree(m))
 dbdt = differentiate(b, x) * f
 
 modes = collect(Set(E) ∩ Set(1:9))
-@variable m μ1[i=1:length(K)] Meas([x;u], support=K[i])
-@variable m μ2[i=1:length(K)] Meas([x;u], support=K[i])
-@variable m μ3[i=1:length(K)] Meas([x;u], support=K[i])
-@objective m Min sum(Mom.(c,μ2))
-cn = @constraint m [i=1:length(K)] Mom.(dbdt,μ2[i]) .== Mom.(b,μ3[i]) - Mom.(b,μ1[i])
-@constraint m [i=modes] sum(μ1[eout(E,i)]) == sum(μ3[einc(E,i)])
-@constraint m sum(μ1[eout(E,10)]) == μ0
-@constraint m sum(μ3[einc(E,11)]) == μT
-@constraint m Mom.(1,μ1) .<= 1
-@constraint m Mom.(1,μ3) .<= 1
+@variable m μ[i=1:length(K),j=1:3] Meas([x;u], support=K[i])
+@objective m Min sum(Mom.(c,μ[:,2]))
+cn = @constraint m [i=1:length(K)] Mom.(dbdt,μ[i,2]) .== Mom.(b,μ[i,3]) - Mom.(b,μ[i,1])
+@constraint m [i=modes] sum(μ[eout(E,i),1]) == sum(μ[einc(E,i),3])
+@constraint m sum(μ[eout(E,10),1]) == μ0
+@constraint m sum(μ[einc(E,11),3]) == μT
+@constraint m Mom.(1,μ[:,1]) .<= 1
+@constraint m Mom.(1,μ[:,3]) .<= 1
 
 optimize!(m)
 
-p = integrate.(1,μ1)
+p = integrate.(1,μ[:,1])
 @show objective_value(m)
 @show p
 
 v = [first.(-dual.(c))'*b for c in cn]
 contourf(range(-1,0,100),range(-1,0,100),(x1,x2)->v[2](x1,x2,0,0.5))
-plot!([-0.7, -0.3, -0.3, -0.7, -0.7], [-0.7, -0.7, -0.3, -0.3, -0.7], color=:black, linestyle=:dash, label=false)
+plot!([-0.7, -0.3, -0.3, -0.7, -0.7], [-0.7, -0.7, -0.3, -0.3, -0.7], linestyle=:dash, label=false)
 savefig("test/gmp2.pdf")
+
+fc(x0, p, t) = let 
+    dv0 = differentiate(v[2], x)
+    dvx0 = [d(x0) for d in dv0]
+    u0 = -B'*dvx0
+    A*x0 + B*u0
+end
+prob = ODEProblem(fc, x0, (0.0, 10.0), dt=0.05)
+sol = solve(prob, Euler())
+#scatter!([Tuple(u[1:2]) for u in sol.u], label=false)
