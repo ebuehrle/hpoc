425c57f06862c45e90e34a08414680ed8ef6a4d6
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2024-05-31 18:41:53 +0200

    wip rover2

--- rover-2.jl
+++ rover-2.jl
@@ -6,14 +6,15 @@ using MosekTools
 using Ipopt
 using Plots
 using .PWA
+using Spot
 
 c(x,u) = x[3:4]'*x[3:4] + x[7:8]'*x[7:8] + u'*u + 0.1
 A1 = [0 0 1 0; 0 0 0 1; 0 0 0 0; 0 0 0 0]
 B1 = [0 0; 0 0; 1 0; 0 1]
 A = [A1 zeros(4,4); zeros(4,4) A1]
 B = [B1 zeros(4,2); zeros(4,2) B1]
-#l = "G(r1a) & G(r2a) & F(r1o1 | r2o1) & F(r1o2 | r2o2) & F(r1o3 | r2o3)"# & F(r1o4 | r2o4) & F(r1t1 | r2t1) & F(r1t2 | r2t2)
-l = "G(r1a) & G(r2a) & G((r1o1 | r1o2 | r1o3) -> F(r1t1 | r1t2)) & G((r2o1 | r2o2 | r2o3) -> F(r2t1 | r2t2)) & F(r1o1 | r2o1) & F(r1o2 | r2o2) & F(r1o3 | r2o3)"# & F(r1o4 | r2o4) & F(r1t1 | r2t1) & F(r1t2 | r2t2)
+#l = "G(ra) & G((r1o1 | r1o2 | r1o3) -> F(r1t1)) & G((r2o1 | r2o2 | r2o3) -> F(r2t1)) & F(r1o1 | r2o1) & F(r1o2 | r2o2) & F(r1o3 | r2o3)"# & F(r1o4 | r2o4) & F(r1t1 | r2t1) & F(r1t2 | r2t2)
+l = "G(ra) & G((r1o1 | r1o2 | r1o3) -> F(r1t1)) & G((r2o1 | r2o2 | r2o3) -> F(r2t1)) & F(r1o1 | r2o1) & F(r1o2 | r2o2) & F(r1o3 | r2o3)"# & F(r1o4 | r2o4) & F(r1t1 | r2t1) & F(r1t2 | r2t2)
 
 V = let x = Symbolics.variables(:x, 1:8)
     r1o1 = HPolyhedron([HalfSpace(-0.4 <= x[1], x), HalfSpace(x[1] <= -0.3, x), HalfSpace(-0.4 <= x[2], x), HalfSpace(x[2] <= -0.3, x)])
@@ -26,36 +27,38 @@ V = let x = Symbolics.variables(:x, 1:8)
     # r2o4 = HPolyhedron([HalfSpace(-0.4 <= x[5], x), HalfSpace(x[5] <= -0.3, x), HalfSpace( 0.3 <= x[6], x), HalfSpace(x[6] <=  0.4, x)])
 
     r1t1 = HPolyhedron([HalfSpace(-0.4 <= x[1], x), HalfSpace(x[1] <= -0.3, x), HalfSpace(-0.05 <= x[2], x), HalfSpace(x[2] <= 0.05, x)])
-    r1t2 = HPolyhedron([HalfSpace( 0.3 <= x[1], x), HalfSpace(x[1] <=  0.4, x), HalfSpace(-0.05 <= x[2], x), HalfSpace(x[2] <= 0.05, x)])
+    #r1t2 = HPolyhedron([HalfSpace( 0.3 <= x[1], x), HalfSpace(x[1] <=  0.4, x), HalfSpace(-0.05 <= x[2], x), HalfSpace(x[2] <= 0.05, x)])
     r2t1 = HPolyhedron([HalfSpace(-0.4 <= x[5], x), HalfSpace(x[5] <= -0.3, x), HalfSpace(-0.05 <= x[6], x), HalfSpace(x[6] <= 0.05, x)])
-    r2t2 = HPolyhedron([HalfSpace( 0.3 <= x[5], x), HalfSpace(x[5] <=  0.4, x), HalfSpace(-0.05 <= x[6], x), HalfSpace(x[6] <= 0.05, x)])
+    #r2t2 = HPolyhedron([HalfSpace( 0.3 <= x[5], x), HalfSpace(x[5] <=  0.4, x), HalfSpace(-0.05 <= x[6], x), HalfSpace(x[6] <= 0.05, x)])
 
     # r1c = HPolyhedron([HalfSpace(-0.2 <= x[1], x), HalfSpace(x[1] <= 0.2, x), HalfSpace(-0.2 <= x[2], x), HalfSpace(x[2] <= 0.2, x)])
     # r2c = HPolyhedron([HalfSpace(-0.2 <= x[5], x), HalfSpace(x[5] <= 0.2, x), HalfSpace(-0.2 <= x[6], x), HalfSpace(x[6] <= 0.2, x)])
     
-    r1a = HPolyhedron([HalfSpace(-1.0 <= x[1], x), HalfSpace(x[1] <= 1.0, x), HalfSpace(-1.0 <= x[2], x), HalfSpace(x[2] <= 1.0, x)])
-    r2a = HPolyhedron([HalfSpace(-1.0 <= x[5], x), HalfSpace(x[5] <= 1.0, x), HalfSpace(-1.0 <= x[6], x), HalfSpace(x[6] <= 1.0, x)])
+    ra = HPolyhedron([
+        HalfSpace(-1.0 <= x[1], x), HalfSpace(x[1] <= 1.0, x), HalfSpace(-1.0 <= x[2], x), HalfSpace(x[2] <= 1.0, x),
+        HalfSpace(-1.0 <= x[5], x), HalfSpace(x[5] <= 1.0, x), HalfSpace(-1.0 <= x[6], x), HalfSpace(x[6] <= 1.0, x),
+    ])
 
     [
         r1o1, r1o2, r1o3,
         r2o1, r2o2, r2o3,
-        r1t1, r1t2,
-        r2t1, r2t2,
-        r1a, r2a
+        r1t1, #r1t2,
+        r2t1, #r2t2,
+        ra,
     ]
 
 end
 O = [
-    ["r1o1", "r1a", "r2a"], ["r1o2", "r1a", "r2a"], ["r1o3", "r1a", "r2a"],
-    ["r2o1", "r1a", "r2a"], ["r2o2", "r1a", "r2a"], ["r2o3", "r1a", "r2a"],
-    ["r1t1", "r1a", "r2a"], ["r1t2", "r1a", "r2a"],
-    ["r2t1", "r1a", "r2a"], ["r2t2", "r1a", "r2a"],
-    ["r1a", "r2a"], ["r1a", "r2a"],
+    ["r1o1", "ra"], ["r1o2", "ra"], ["r1o3", "ra"],
+    ["r2o1", "ra"], ["r2o2", "ra"], ["r2o3", "ra"],
+    ["r1t1", "ra"], #["r1t2", "ra"],
+    ["r2t1", "ra"], #["r2t2", "ra"],
+    ["ra"],
 ]
 x0 = [-0.1, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0]
 xT = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
-s, q0, qT = PPWA(A, B, l, V=V, O=O, merge_modes=false)
+s, q0, qT = PPWA(A, B, l, LTLTranslator(deterministic=true, state_based_acceptance=true, buchi=true), V=V, O=O, merge_modes=false)
 println(HybridSystems.nmodes(s), " modes")
 println(HybridSystems.ntransitions(s), " transitions")
 println(q0)
@@ -73,7 +76,7 @@ plot!([ 0.3,  0.4,  0.4,  0.3,  0.3], [ 0.3,  0.3,  0.4,  0.4,  0.3], color=:gre
 #plot!([-0.4, -0.3, -0.3, -0.4, -0.4], [ 0.3,  0.3,  0.4,  0.4,  0.3], color=:green, linestyle=:dash, fill=true, fillalpha=0.2, label=false)
 
 plot!([-0.4, -0.3, -0.3, -0.4, -0.4], [-0.05, -0.05, 0.05, 0.05, -0.05], color=:yellow, linestyle=:dash, fill=true, fillalpha=0.2, label=false)
-plot!([ 0.3,  0.4,  0.4,  0.3,  0.3], [-0.05, -0.05, 0.05, 0.05, -0.05], color=:yellow, linestyle=:dash, fill=true, fillalpha=0.2, label=false)
+#plot!([ 0.3,  0.4,  0.4,  0.3,  0.3], [-0.05, -0.05, 0.05, 0.05, -0.05], color=:yellow, linestyle=:dash, fill=true, fillalpha=0.2, label=false)
 
 #plot!([-0.2, 0.2, 0.2, -0.2, -0.2], [-0.2, -0.2, 0.2, 0.2, -0.2], color=:blue, linestyle=:dash, fill=true, fillalpha=0.2, label=false)
 
--- test/doorpuzzle2.jl
+++ test/doorpuzzle2.jl
@@ -1,5 +1,13 @@
 using Spot
 
-f = ltl"G room & G!w1 & G!w2 & G!w3 & G!w4 & (!d5 U k5) & (!d4 U k4)"
+#f = ltl"G room & G!w1 & G!w2 & G!w3 & G!w4 & (!d5 U k5) & (!d4 U k4)"
+#f = ltl"G((a1 | a2) -> F(b1 | b2))"
+#f = ltl"G(r1a) & G(r2a) & G((r1o1 | r1o2 | r1o3) -> F(r1t1 | r1t2)) & G((r2o1 | r2o2 | r2o3) -> F(r2t1 | r2t2)) & F(r1o1 | r2o1)"# & F(r1o2 | r2o2) & F(r1o3 | r2o3)"# & F(r1o4 | r2o4) & F(r1t1 | r2t1) & F(r1t2 | r2t2)#
+#f = ltl"(G(a -> F(b))) & (G(c -> F(d)))"
+#f = ltl"G((a1 | a2) -> F(b)) & G(c -> F(d))"
+#f = ltl"G(r1a) & G(r2a) & G((r1o1 | r1o2 | r1o3) -> F(r1t1 | r1t2)) & G((r2o1 | r2o2 | r2o3) -> F(r2t1 | r2t2)) & F(r1o1 | r2o1) & F(r1o2 | r2o2) & F(r1o3 | r2o3)"# & F(r1o4 | r2o4) & F(r1t1 | r2t1) & F(r1t2 | r2t2)
+#f = ltl"G((a1|a2) -> F(b1|b2)) & F(a1|a2)"
 #f = ltl"G! w"
-a = translate(LTLTranslator(), f)
+f = ltl"G(a -> F(b))"
+a = translate(LTLTranslator(deterministic=true, state_based_acceptance=true, buchi=true), f)
+get_rabin_acceptance(a)
