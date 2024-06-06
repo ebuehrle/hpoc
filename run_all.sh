19b8ab17c51ea0430cc87242f20243ca6429b361
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2024-06-06 21:12:31 +0200

    add experiments to run script

--- run_all.sh
+++ run_all.sh
@@ -4,3 +4,5 @@ julia --project doorpuzzle-1.jl > doorpuzzle-1.log
 julia --project rover-2.jl > rover-2.log
 julia --project stlcg-1-miqp.jl > stlcg-1-miqp.log
 julia --project stlcg-2-miqp.jl > stlcg-2-miqp.log
+julia --project roundabout-1.jl > roundabout-1.log
+julia --project merge-1.jl > merge-1.log
