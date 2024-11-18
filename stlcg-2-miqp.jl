35a6af510004da7fa18464562fdc7b0e54ec3cb9
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2024-11-18 21:30:40 +0100

    font

--- stlcg-2-miqp.jl
+++ stlcg-2-miqp.jl
@@ -3,7 +3,7 @@ using Symbolics, LazySets
 using HybridSystems
 using JuMP
 using Gurobi
-using Plots; ENV["GKSwstype"] = "100"
+using Plots; ENV["GKSwstype"] = "100"; default(fontfamily="Computer Modern", framestyle=:box)
 using .PWA
 
 c(x,u) = x'*x + u'*u
@@ -33,5 +33,7 @@ plot!([-0.9, -0.8, -0.8, -0.9, -0.9], [-0.6, -0.6, -0.4, -0.4, -0.6], linestyle=
 plot!([-0.6, -0.4, -0.4, -0.6, -0.6], [-0.6, -0.6, -0.4, -0.4, -0.6], linestyle=:dash, color=:blue, fill=true, fillalpha=0.2, label=false)
 plot!([-0.5, -0.2, -0.2, -0.5, -0.5], [-0.9, -0.9, -0.8, -0.8, -0.9], linestyle=:dash, color=:red, fill=true, fillalpha=0.2, label=false)
 plot!([-0.5, -0.25, -0.25, -0.5, -0.5], [-0.25, -0.25, -0.0, -0.0, -0.25], linestyle=:dash, color=:green, fill=true, fillalpha=0.2, label=false)
-plot!([-1.1,  0.1,  0.1, -1.1, -1.1], [-1.1, -1.1,  0.1,  0.1, -1.1], color=:black, label=false)
+#plot!([-1.1,  0.1,  0.1, -1.1, -1.1], [-1.1, -1.1,  0.1,  0.1, -1.1], color=:black, label=false)
+xlims!(-1.15, 0.15)
+ylims!(-1.15, 0.15)
 savefig("out/stlcg-2-miqp.pdf")
